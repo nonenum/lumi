7297215c6288ddfd8d8d55af2ddb8bad8c50da13
nonenum <nagyvaradiabel@hotmail.com>
2026-02-14 18:28:50 +0100

    Movement

--- README.md
+++ README.md
@@ -1,7 +1,19 @@
 # Lumi
 ### Basic GTK4 Desktop Pet
 
-My first Linux specific program, made for personal use.
+My first Linux specific program, made for personal use, and entertainment.
+
+### Requirements
+
+This program forces the legacy x11
+
+You need to install **wmctrl** for your distribution in order to run Lumi
+
+In Fedora's case use:
+
+```bash
+  sudo dnf install wmctrl
+```
 
 ## Disclaimer
 
--- main.py
+++ main.py
@@ -1,12 +1,16 @@
-from gi.repository import Gtk, Gdk, GLib
+import os
+
+os.environ["GDK_BACKEND"] = "x11"
+
 import sys
 import gi
+import time
 
 gi.require_version('Gtk', '4.0')
+from gi.repository import Gtk, Gdk, GLib
 
-
-g_application_version = "a1.0"
-g_code_name = "Unshattered"
+g_application_version = "a1.1"
+g_code_name = "Inertia"
 
 
 class Lumi(Gtk.ApplicationWindow):
@@ -15,16 +19,30 @@ class Lumi(Gtk.ApplicationWindow):
 
         self.set_title("Lumi")
         self.set_decorated(False)
+        self.set_titlebar(None)
 
         css_provider = Gtk.CssProvider()
         css_provider.load_from_data(b"""
-                window { background-color: transparent; }
-                image { background-color: transparent; }
-                windowhandle { background-color: transparent; }
+                window {
+                    background-color: transparent;
+                    box-shadow: none;
+                }
+                image {
+                    background-color: transparent;
+                    box-shadow: none;
+                }
+                decoration {
+                    box-shadow: none;
+                    background: none;
+                }
+                windowhandle {
+                    background-color: transparent;
+                    box-shadow: none;
+                    border: none;
+                }
             """)
 
-        Gtk.StyleContext.add_provider_for_display(Gdk.Display.get_default(
-        ), css_provider, Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION)
+        Gtk.StyleContext.add_provider_for_display(Gdk.Display.get_default(), css_provider, Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION)
 
         self.image = Gtk.Image.new_from_file("res/idle.png")
         self.image.set_pixel_size(300)
@@ -32,6 +50,8 @@ class Lumi(Gtk.ApplicationWindow):
         self.handle = Gtk.WindowHandle()
         self.handle.set_child(self.image)
         self.set_child(self.handle)
+
+        self.is_dragging = False
 
         click_controller = Gtk.GestureClick()
         click_controller.set_button(0)
@@ -49,7 +69,43 @@ class Lumi(Gtk.ApplicationWindow):
 
         self.handle.add_controller(motion_controller)
 
+        self.pos_x = 100
+        self.pos_y = 300
+        self.walk_direction = 1
+
         self.present()
+
+        GLib.timeout_add(1000, self.force_always_on_top)
+        GLib.timeout_add(50, self.auto_walk)
+
+    def auto_walk(self):
+        if self.is_dragging:
+            return True
+
+        speed = 2
+
+        self.pos_x += speed * self.walk_direction
+
+        if self.pos_x > 500:
+            self.walk_direction = -1
+        elif self.pos_x < 100:
+            self.walk_direction = 1
+
+        try:
+            cmd = f"wmctrl -r 'Lumi' -e 0,{self.pos_x},{self.pos_y},-1,-1"
+            os.system(cmd)
+        except:
+            pass
+
+        return True
+
+    def force_always_on_top(self):
+        try:
+            print("[LUMI]: Trying to force pin...")
+            os.system("wmctrl -r 'Lumi' -b add,above")
+        except Exception as e:
+            print(f"[LUMI]: Failed to pin window: {e}")
+        return False
 
     def on_pressed(self, gesture, n_press, x, y):
         self.image.set_from_file("res/grabbed.png")
@@ -57,13 +113,11 @@ class Lumi(Gtk.ApplicationWindow):
     def on_status_check(self, controller, x, y):
         event = controller.get_current_event()
 
-        if event is None:
-            return
+        if event:
+            state = event.get_modifier_state()
 
-        state = event.get_modifier_state()
-
-        if not (state & Gdk.ModifierType.BUTTON1_MASK):
-            self.image.set_from_file("res/idle.png")
+            if not (state & Gdk.ModifierType.BUTTON1_MASK):
+                self.image.set_from_file("res/idle.png")
 
 
 class LumiApplication(Gtk.Application):
